21020341b6372694d40a5ed9cd10faf5843c6319
Justin Lambert <jlambert@eml.cc>
2013-04-03 09:05:54 -0600

    add sensu graphite monitoring

---
+++ manifests/monitoring/sensu.pp
@@ -0,0 +1,14 @@
+class redis::monitoring::sensu {
+
+  sensu::check { 'redis-metrics':
+    type        => 'metric',
+    handlers    => 'graphite',
+    standalone  => true,
+    subscribers => ['redis'],
+    command     => '/etc/sensu/plugins/redis-metrics.rb -h localhost -p 6379',
+    interval    => 30,
+  }
+
+  sensu::subscription { 'redis': }
+
+}
